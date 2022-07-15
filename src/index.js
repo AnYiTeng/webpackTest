ea4770eeb12557c5c0cf1f7a777613e6b8ce2339
wb-ayt773841 <wb-ayt773841@alibaba-inc.com>
2022-07-15 17:20:00 +0800

    feat: 各类文件loader加载

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import _ from 'lodash'
+import Icon from './icon.png'
+import Data from './data.xml'
+import Notes from './data.csv'
+import DataJson, { name } from './data.json'
+import './style.css'
 
 function component() {
     const ele = document.createElement('div')
     // lodash 在当前 script 中使用 import 引入
     ele.innerHTML = _.join(['Hellow', 'Webpack'], ' ')
+    ele.classList.add('hello')
+
+    const myIcon = new Image()
+    myIcon.src = Icon
+    ele.appendChild(myIcon)
+
+    console.log(Data)
+    console.log(Notes)
+    console.log(DataJson, name)
+
     return ele
 }
 
