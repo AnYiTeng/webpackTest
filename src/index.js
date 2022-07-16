bf828cd76f0c14471def9c024263752dd649acf3
AnYiteng <240938530@qq.com>
2022-07-16 17:00:02 +0800

    feat: 代码格式

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import _ from 'lodash'
 import printMe from './print.js'
 
 function component() {
-    const ele = document.createElement('div')
-    const btn = document.createElement('button')
+	const ele = document.createElement('div')
+	const btn = document.createElement('button')
 
-    // lodash 在当前 script 中使用 import 引入
-    ele.innerHTML = _.join(['Hellow', 'Webpack'], ' ')
+	// lodash 在当前 script 中使用 import 引入
+	ele.innerHTML = _.join(['Hellow', 'Webpack'], ' ')
 
-    btn.innerHTML = 'Click me and check the console!'
-    btn.onclick = printMe
+	btn.innerHTML = 'Click me and check the console!'
+	btn.onclick = printMe
 
-    ele.appendChild(btn)
+	ele.appendChild(btn)
 
-    return ele
+	return ele
 }
 
 document.body.appendChild(component())
--- webpack.config.js
+++ webpack.config.js
@@ -3,31 +3,31 @@ const TerserPlugin = require('terser-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: {
-        index: './src/index.js',
-        print: './src/print.js'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: '管理输出'
-        }),
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    },
-    resolve: {
-        alias: {
-            ayt: path.resolve(__dirname, 'src'),
-        }
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new TerserPlugin({
-                extractComments: false, // webpack5 打包后会自动生成.txt文件，此配置不将注释提取到单独的文件中
-            })
-        ]
-    }
+	entry: {
+		index: './src/index.js',
+		print: './src/print.js'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: '管理输出'
+		}),
+	],
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true
+	},
+	resolve: {
+		alias: {
+			ayt: path.resolve(__dirname, 'src'),
+		}
+	},
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new TerserPlugin({
+				extractComments: false, // webpack5 打包后会自动生成.txt文件，此配置不将注释提取到单独的文件中
+			})
+		]
+	}
 }
