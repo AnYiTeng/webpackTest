56e8907d509b68561a131e1887e412ccd800867a
wb-ayt773841 <wb-ayt773841@alibaba-inc.com>
2022-07-19 09:38:39 +0800

    feat: esmodule导出

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,23 @@ const path = require('path')
 
 module.exports = {
 	entry: './src/index.js',
+	experiments: {
+    outputModule: true,
+  },
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'webpack-numbers.js',
 		library: {
-			name: 'webpackNumbers',
 			type: 'module'
 		},
 		clean: true
 	},
-	externals: {
-		lodash: { // 这意味着你的 library 需要一个名为 lodash 的依赖，这个依赖在 consumer 环境中必须存在且可用。
-			commonjs: 'lodash',
-			commonjs2: 'lodash',
-			amd: 'lodash',
-			root: '_'
-		}
-	}
+	// externals: {
+	// 	lodash: { // 这意味着你的 library 需要一个名为 lodash 的依赖，这个依赖在 consumer 环境中必须存在且可用。
+	// 		commonjs: 'lodash',
+	// 		commonjs2: 'lodash',
+	// 		amd: 'lodash',
+	// 		root: '_'
+	// 	}
+	// }
 }
