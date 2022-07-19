2209ea2228b7b0841b0181264a292901f0511b22
wb-ayt773841 <wb-ayt773841@alibaba-inc.com>
2022-07-19 10:39:27 +0800

    feat: 环境变量

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import _ from 'lodash'
-// import Print from './print.js'
 
 function component() {
 	const el = document.createElement('div')
-
-	// lodash 是由当前 script 脚本 import 进来的
 	el.innerHTML = _.join(['Hello', 'Webpack'], ' ')
-	// el.onclick = Print.bind(null, 'hello webpack')
-
 	return el
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,15 @@
-/**
- * @type {import('webpack').Configuration}
-*/
-
 const path = require('path')
-const TerserPlugin = require('terser-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-	mode: 'development',
-	entry: {
-		index: './src/index.js',
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		static: './dist'
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			title: 'Caching'
-		}),
-	],
-	output: {
-		filename: '[name].[contenthash].js',
-		path: path.resolve(__dirname, 'dist'),
-		clean: true,
-	},
-	resolve: {
-		alias: {
-			ayt: path.resolve(__dirname, 'src'),
-		}
-	},
-	optimization: {
-		minimize: true,
-		minimizer: [
-			new TerserPlugin({
-				extractComments: false, // webpack5 打包后会自动生成.txt文件，此配置不将注释提取到单独的文件中
-			})
-		],
-		moduleIds: 'deterministic',
-		runtimeChunk: 'single',
-		splitChunks: {
-			cacheGroups: {
-				vendor: {
-					test: /[\\/]node_modules[\\/]/,
-					name: 'vendors',
-					chunks: 'all'
-				}
-			}
-		}
+module.exports = (env) => {
+	console.log('Goal:', env.goal)
+	console.log('Production:', env.production)
+
+	return {
+		entry: './src/index.js',
+		output: {
+			filename: 'bundle.js',
+			path: path.resolve(__dirname, 'dist'),
+			clean: true
+		},
 	}
 }
