a9ce85b6fefa81705b3f9b09226901d3f0394ac9
wb-ayt773841 <wb-ayt773841@alibaba-inc.com>
2022-07-21 10:49:34 +0800

    feat: 生产环境配置

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import _ from 'lodash'
+import chalk from 'chalk'
+
+if (process.env.NODE_ENV !== 'production') {
+	console.log(chalk.bgBlueBright('Looks like we are in development mode!'))
+}
 
 function component() {
 	const el = document.createElement('div')
 
-	el.innerHTML = _.join(['Hello', 'Webpack'], ' ')
+	el.innerHTML = _.join(['Hello!!', 'Webpack'], ' ')
 
 	return el
 }
